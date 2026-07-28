642742b2d6fe6c36ebe43a44a119d7d55feaa181
saddalasurendra9-cmyk <saddalasurendra9@gmail.com>
2026-07-28 15:37:32 +0530

    first commit

---
+++ day1.py
@@ -0,0 +1,14 @@
+'''
+print('Hello codegnan')
+
+'''
+#name = 'codegnan'
+#print(name)
+#email_id ='surendralohit0@gmail.com'
+#4328print(email_id)
+1ower = 22
+#multiassignment of variables
+name = 'codegnan'
+print(name)
+
+name,location,age = 'codegnan','hyd',7
