c897fabd043fccd7b048c73677cf8b202fa98a24
saddalasurendra9-cmyk <saddalasurendra9@gmail.com>
2026-08-04 08:33:45 +0530

    Task

---
+++ task.py
@@ -0,0 +1,36 @@
+#Grade CHECKER
+'''
+marks = int(input("enter the marks:"))
+if marks >100 or marks <0:
+    print("invalid marks entered")
+elif marks >=90:
+    print("Grade: A")
+    print("Remark: outstanding")
+elif marks >=80:
+    print("Grade: B")
+    print("Remark: Execellent")
+elif marks >=70:
+    print("Grade: c")
+    print("Remark: Good")
+elif marks >=60:
+    print("Grade: D")
+    print("Remark: Fail, needs improvement")
+elif marks >=50:
+    print("Grade: E")
+    print("Remark: poor,needs serious improvement")
+else:
+    print("Grade: Fail")
+    print("Remark:faild ,needs to reappear")
+#Even -0dd
+num = int(input("enter a number"))
+if num == 0:
+          print("zero is neither even nor odd")
+elif num < 0 and num %2 == 0:
+    print("Negative even number")
+elif num < 0 and num %2 != 0:
+    print("Negative odd number")
+elif num > 0 and num %2 == 0:
+    print(" even number")
+else:
+    print("odd Number")
+'''
