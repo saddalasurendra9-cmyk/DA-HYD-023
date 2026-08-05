53e23b42f0a42e23c847b82b0a84cd95a1316d9c
saddalasurendra9-cmyk <saddalasurendra9@gmail.com>
2026-08-05 19:21:40 +0530

    stringusage

---
+++ day8.py
@@ -0,0 +1,215 @@
+'''
+Tokens  --> keywords ,Identifiers,literals,operators,puncutuators,variables,
+operators -->Numeric data (int,float,complex,),bool
+control flow  -->if,elif,else,for,while
+sequences -->strings,lists,sets,tuples,mappinga(dict)
+
+#Strings -->group of charaters,we use single or double or triple quotes
+#for representation of strings...
+#strings are immutuable  ,ordered,indexed collection
+#space is also a charater
+name = 'codegnan'
+print(name)
+print(type(name))
+print(len(name)) #len --> returns the number of items in container
+#index() -->fetch the object (position) starts at 0 and ends at len(obj)-
+
+#we use []representation
+print(name[0])
+print(name[3])
+print(name[25])#indexError --> as its out of range
+#Negative Indexing --> -1 to len(obj)
+print(name[-1])#it returns last charater
+print(name[-2])
+#print(name[-33]) #indexerror
+
+#slicing --> we can access group of charater(objects)
+#we use [start:end] #start default  --> 0,start is included ,end is excluded
+name = 'surendra'
+print(name[:])#returns entire string
+print(name[0:])#returns entire string
+print(name[:4])#starts at 0th inddex before 4th index
+print(name[1:5])
+print(name[4:])
+
+name ='python'
+print(name[3:7])
+print(name[7:3])#returns empty as strings are immutable
+#slicing is applicable from lower index to higher index
+print(name[45:])
+
+name = 'surendra'
+print(name[-1:-5])#returns empty string
+print(name[-5:-1])#starts at -5 and ends at -2
+#print 'on' from above string
+print(name[4:])
+print(name[4:6])
+print(name[-2:])
+
+name = 'surendra'
+print(name[1:-2])
+print(name[2:-6])
+#strinding --> [start:end:step]
+course =  'DataAnalysis'
+print(len(course))
+#Data --> result
+print(course[:4])
+print(course[4:])
+
+print(course[::1])#returns all  charaters
+print(course[::2])#includes start to end skipping1 charater
+print(course[1:6:3])#[1:6]--> ataAn -->[1:6:3]-->aA
+#tnys
+print(course[2::3])
+print(course[::-1])#it returns the reverse of a string
+print(course[::-2])
+
+#task: workout with all possibilities of slicing and striding on a example
+name = 'codegnan'
+#name[3] = 'w' #strings are immutable
+#operators on strings  --> Indexing ,concatenation, repetition
+print(name * 3 )
+print('*' * 25)#repetiton
+#concatenation -> combining strings
+
+data = 'saketh' + 'python' + ''+'database'
+print(data)
+print('123') * 4) #Numeric string
+print('code in 'codegnan')
+
+for i in 'codegnan':
+      print(i,':')
+#in above case we gets every charater line by line
+      
+for i in 'codegnan':
+    print(i,end=' ,')
+    
+name = "dataCodegnan"
+#Built-in functions --> len(),max(),sorted()
+print(len(name))
+print(min(name)) #alphabetical order ASCII ordering
+print(ord('A'))
+print(ord('a'))
+print(chr(97))
+print(max(name))
+print(sorted(name))#returns a list by sorting all elements
+
+#Methods on strings --> case-conversions,finding/searching..
+name = 'codEgnan data'
+#case-conversions  -->upper(),lower(),title(),capitalize()
+a = name.upper()
+print(a)
+b = name.lower()
+print(b)
+#Capitalize() --> convert first letter to uppercase
+c = name,Capitalize()
+print(c)
+d = name.title()#convert every work first letter to uppercase
+print(d)
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
